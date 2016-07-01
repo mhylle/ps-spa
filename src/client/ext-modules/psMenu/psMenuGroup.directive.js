d3ad27519263932ce620340c977be11b9d946e41
mhylleberg@csc.com <mhylle@yahoo.com>
2016-07-01 14:17:50 +0200

    starting on grouping module

---
+++ src/client/ext-modules/psMenu/psMenuGroup.directive.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('psMenu').directive('psMenuGroup', function () {
+  return {
+    require: '^psMenu',
+    transclusion: true,
+    scope: {
+      label: '@',
+      icon: '@'
+    },
+    restrict: 'AE',
+    templateUrl: 'ext-modules/psMenu/psMenuGroupTemplate.html',
+
+    link: function (scope, el, attr, ctrl) {
+      // scope.isActive = function(){
+      //   return el === ctrl.getActiveElement();
+      // };
+      // el.on('click', function (evt) {
+      //   evt.stopPropagation();
+      //   evt.preventDefault();
+      //   scope.$apply(function () {
+      //     ctrl.setActiveElement(el);
+      //     ctrl.setRoute(scope.route);
+      //   });
+      // })
+    }
+  };
+});
